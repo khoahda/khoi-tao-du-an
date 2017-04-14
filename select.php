aa1888f0c79b47721de0af746d3b2ebb63980aa0
khoahda <idofkt1811@gmail.com>
2017-04-14 10:44:37 +0700

    Add files via upload

---
+++ select.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<table>
+<tr>
+	<td>
+		<select style="width:200px">
+			<option value="">--Chon ngay--</option>
+		</select>
+	</td>
+</tr>
+<tr>
+	<td>
+		<select style="width:200px">
+			<option value="">--Chon thang--</option>
+		</select>
+	</td>
+</tr>
+</table>
+</body>
+</html>
